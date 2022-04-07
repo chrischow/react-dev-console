3a9208c58de97988cf3829041be311535ce4ae15
Chris Chow <christian.chow.en.yi@gmail.com>
2022-04-07 10:07:37 +0800

    Add info modal

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import './App.css';
 import $ from 'jquery';
 
-
+// Save original console.log for debugging
 console.Log = console.log;
 
+// Cell output component: appears below code cell
 function CellOutput(props) {
   
   // Overwrite console.log
@@ -59,6 +60,7 @@ function CellOutput(props) {
   return outputs
 }
 
+// Cell component: code + output
 function Cell(props) {
   // Initialise states: `form` for controlled form and `cell` for cell value
   const [cell, setCell] = React.useState('');
@@ -117,6 +119,7 @@ function Cell(props) {
             name="commands"
             value={form.commands}
             onChange={handleChange}
+            placeholder="Input your code here"
             rows="3"
           >
           </textarea>
@@ -140,17 +143,72 @@ function Cell(props) {
   );
 }
 
+// Main app component
 function App() {
   return (
     <div className="container mt-4">
       <h1 className="text-center">React Dev Console</h1>
-      <p className="subtitle text-center mb-4">
-        A workaround console for debugging your code in a tool-less environment.
+      <p className="subtitle text-center mb-4 align-items-center">
+        <span className="mr-2">
+          A tool for testing your code in a limiting IT environment.
+        </span>
+        <a data-toggle="modal" data-target="#infoModal">
+          <svg 
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+            role="img"
+            className="play-button mb-1"
+          >
+            <path 
+              fill="currentColor"
+              d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 
+              256-256S397.4 0 256 0zM256 400c-18 0-32-14-32-32s13.1-32 32-32c17.1 
+              0 32 14 32 32S273.1 400 256 400zM325.1 258L280 286V288c0 13-11 24-24 
+              24S232 301 232 288V272c0-8 4-16 12-21l57-34C308 213 312 206 312 
+              198C312 186 301.1 176 289.1 176h-51.1C225.1 176 216 186 216 198c0 
+              13-11 24-24 24s-24-11-24-24C168 159 199 128 237.1 128h51.1C329 128 
+              360 159 360 198C360 222 347 245 325.1 258z"
+            />
+          </svg>
+        </a>
       </p>
       <Cell id="1" />
-      {/* <div className="text-center mt-5 mb-5">
-        <button className="btn btn-sm btn-blue">Add Cell</button>
-      </div> */}
+      <div className="modal fade" id="infoModal" tabindex="-1" role="dialog" aria-labelledby="infoModalLabel" aria-hidden="true">
+        <div className="modal-dialog" role="document">
+          <div className="modal-content bg-dark">
+            <div className="modal-header">
+              <h5 className="modal-title" id="infoModalLabel">About This Project</h5>
+              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">
+              <p>
+                <span className="text-green">React Dev Console </span>  
+                is a tool built in React.js to enable testing of JS code in the browser.
+              </p>
+              <p>
+                Simply paste your code into the code cell and hit 
+                <span className="text-blue"> Ctrl + Enter</span> or click the
+                play button 
+                <svg className="ml-1 play-button mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" role="img">
+                  <path 
+                    fill="currentcolor"
+                    d="M361 215C375.3 223.8 384 239.3 384 256C384 272.7 375.3 288.2 361 
+                    296.1L73.03 472.1C58.21 482 39.66 482.4 24.52 473.9C9.377 465.4 0 
+                    449.4 0 432V80C0 62.64 9.377 46.63 24.52 38.13C39.66 29.64 58.21 
+                    29.99 73.03 39.04L361 215z"
+                  />
+                </svg>
+                to run your code.
+              </p>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
